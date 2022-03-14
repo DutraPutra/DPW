b6997a123a9d137cacc284a1ab731d93b472db1f
DutraPutra <dutraproject2021@gmail.com>
2022-03-14 17:23:47 +0700

    first commit

---
+++ frontend/scripts/scripts.js
@@ -0,0 +1,7 @@
+const h1 = document.getElementsById("hai")
+h1.style.color ="green";
+h1.style.background = "black";
+
+const h2 = Document.getElementsByTagName("p");
+h2[0].style.color = "green";
+h2[0].style.background = "black";
